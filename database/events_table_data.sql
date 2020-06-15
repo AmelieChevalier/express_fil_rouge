2f586a6691d88f0aa9b0fbaa0fb881eb1cb1bdaa
Amélie <ameliechevalier3006@gmail.com>
2020-06-15 11:21:57 +0200

    first routes GET

--- database/events_table_data.sql
+++ database/events_table_data.sql
@@ -1,6 +1,9 @@
 INSERT INTO events (name, date, is_public, max_guests) VALUES
-('Solidays', '2020-06-23', 1, 20000),
+('Solidays Festival', '2020-06-23', 1, 20000),
 ('Festival d''Avignon', '2020-08-12', 1, 1000),
 ('Mariage', '2020-10-24', 0, 150),
 ('Week-end piscine', '2020-09-03', 0, 10),
-('Représentation théâtre', '2020-07-14', 1, 250);
+('Représentation théâtre', '2020-07-14', 1, 250),
+('Anniversaire Amélie', '2021-06-30', 0, 25),
+('Déménagement', '2019-10-12', 0, 3),
+('Anniversaire Elodie', '2021-06-10', 0, 30);
--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const express = require('express');
+const connection = require('./conf');
 const app = express();
 const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur mon application de gestion d\'événements !')
+})
+
+// Récupération de l'ensemble des données de la table
+
+app.get('/api/events', (req, res) => {
+    connection.query('SELECT * FROM events', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des événements');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Récupération du nom et de la date des événements
+
+app.get('/api/events/resume', (req, res) => {
+  connection.query('SELECT id, name, date FROM events', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des noms et dates');
+    } else {
+      res.json(results);
+    }
+  })
+})
+
+// Récupération des données "contient"
+
+app.get('/api/events/contains/:search', (req, res) => {
+    console.log(req.params.search);
+    connection.query('SELECT * FROM events WHERE is_public = ?', req.params.search, (err, results) => {
+      if (err) {
+        res.status(500).send('Erreur lor de la récupération des événements');
+      } else {
+        res.json(results);
+      }
+    });
+})
+
+// Récupération des données "commence par"
+
+// Récupération des données "supérieur à "
 
 app.listen(port, (err) => {
   if (err) {
