aeddcffaf0ed0634f3a60407a2e4bbefaa7c475e
Amélie <ameliechevalier3006@gmail.com>
2020-06-15 14:25:23 +0200

    routes get

--- index.js
+++ index.js
@@ -34,19 +34,55 @@ app.get('/api/events/resume', (req, res) => {
 // Récupération des données "contient"
 
 app.get('/api/events/contains/:search', (req, res) => {
-    console.log(req.params.search);
-    connection.query('SELECT * FROM events WHERE is_public = ?', req.params.search, (err, results) => {
+    const sqlValues = `%${req.params.search}%`;
+    connection.query('SELECT * FROM events WHERE name LIKE ?', sqlValues, (err, results) => {
       if (err) {
         res.status(500).send('Erreur lor de la récupération des événements');
       } else {
-        res.json(results);
+        if (results.length === 0) {
+          res.status(404).send('Il n\'existe pas d\'événements contenant cette chaîne de caractères')
+        } else {
+          res.json(results);          
+        }
+
       }
     });
 })
 
 // Récupération des données "commence par"
 
+app.get('/api/events/startwith/:search', (req, res) => {
+  const sqlValues = `${req.params.search}%`;
+  connection.query('SELECT * FROM events WHERE name LIKE ?', sqlValues, (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des événements');
+    } else {
+      if (results.length === 0) {
+        res.status(404).send('Il n\'existe pas d\'événements commençant par cette chaîne de caractères !')
+      } else {
+        res.json(results);
+      }
+      
+    }
+  });
+});
+
 // Récupération des données "supérieur à "
+
+app.get('/api/events/superiorto/:date', (req, res) => {
+  const sqlValues = `${req.params.date}`;
+  connection.query('SELECT * FROM events WHERE date > ?', sqlValues, (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des événements');
+    } else {
+      if (results.length === 0) {
+        res.status(404).send('Il n\'y a pas d\'événements après cette date !')
+      } else {
+      res.json(results);
+      }
+    }
+  });
+});
 
 app.listen(port, (err) => {
   if (err) {
